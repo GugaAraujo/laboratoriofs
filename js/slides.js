9f94302d8e17980bdfe1d1947fb3143297453cba
Gustavo Araújo <gustavo.avk@msn.com>
2022-02-17 23:24:59 -0300

    add: arquivos iniciais

---
+++ js/slides.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2200,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+        
+    }
+})
